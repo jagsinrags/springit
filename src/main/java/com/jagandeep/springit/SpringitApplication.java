ce4514ac1948faef76917d8c7b6a23c69805f6c0
Jagandeep Sooden <jagandeep.sooden@gmail.com>
2018-12-03 04:47:29 -0500

    test

--- src/main/java/com/jagandeep/springit/SpringitApplication.java
+++ src/main/java/com/jagandeep/springit/SpringitApplication.java
@@ -8,5 +8,7 @@ public class SpringitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
+		
+		System.out.println("jjjjjjjjxfgjjjjjjj");
 	}
 }
